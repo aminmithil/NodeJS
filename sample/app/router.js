6f69175e547f606392467fadaf7614992ec5e570
Mithil Amin <Mithil Amin>
2016-11-09 07:55:27 -0600

    Added Local Sign In

--- sample/app/router.js
+++ sample/app/router.js
@@ -5,6 +5,16 @@ module.exports = function(app, passport){
         res.render('index.ejs');
     });
 
+    app.get('/login', function(req, res){
+        res.render('login.ejs', { message: req.flash('loginMessage')});
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
+
     app.get('/signup', function(req,res){
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
@@ -14,4 +24,20 @@ module.exports = function(app, passport){
        failureRedirect: '/signup',
        failureFlash: true 
     }));
+
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/profile', isLoggedIn, function(req, res){
+        res.render('profile.ejs', { user: req.user });
+    });
+};
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
 }
